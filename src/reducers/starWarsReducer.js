17ad17b085b0cf3dd9d09900f2a326dca00f9984
raberin <rmvaberin@gmail.com>
2019-04-16 20:06:10 -0700

    Filled in missing code in ./reducers/starWarsReducer.js

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,9 @@
 import { FETCHING, SUCCESS, FAILURE } from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  isLoading: false,
+  error: null
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +11,29 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      //Loading is set to true and is grabbing data from api
+      return {
+        ...state,
+        error: "",
+        isLoading: true
+      };
+    case SUCCESS:
+      //If data is grabbed, isLoading set to false and characted set to action.payload
+      //to render data
+      return {
+        ...state,
+        characters: action.payload,
+        isLoading: false
+      };
+    case FAILURE:
+      //If axios call fails, isLoading is set to false, and error is set to action.payload
+      //to display error
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
     default:
       return state;
   }
